f3534e2d4ede504dd4566753d16f09ecffb5a865
Sumit Rao <sumitraog17es.iot@gmail.com>
2026-06-30 22:38:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -240,23 +240,32 @@ if st.session_state.mode == "Connect Nodes":
             "Capacitor",
             "Inductor",
             "LED",
+            "IC",
             "Diode",
-            "Jumper",
             "Switch",
         ],
     )
 
-        if connection_type == "Trace":
-             expected_resistance = 0
+        if connection_type in ["Trace", "Jumper"]:
+           expected_resistance = 0
 
-        elif connection_type == "Capacitor":
-             expected_resistance = "OPEN"
+       elif connection_type == "Capacitor":
+            expected_resistance = "OPEN"
 
-        else:
-            expected_resistance = st.sidebar.text_input(
-            "Expected Resistance (Ω)",
-            value="1000",
-        )
+       elif connection_type in ["LED", "Diode"]:
+            expected_resistance = "DIODE"
+
+       elif connection_type == "Switch":
+            expected_resistance = st.sidebar.selectbox(
+             "Default State",
+              ["OPEN", "0"]
+              )
+
+elif connection_type == "Resistor":
+    expected_resistance = st.sidebar.text_input(
+        "Resistance (Ω)",
+        value="1000"
+    )
 
         if st.sidebar.button("Save Connection"):
 
